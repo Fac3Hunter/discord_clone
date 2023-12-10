076fb509432ec6f3d4fffad04fc3245d5e778755
Fac3Hunter <facehunt811@gmail.com>
2023-12-10 14:06:51 +0300

    Dark/Light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
       <h1 className="text-4xl font-bold text-orange-600 text-center">
         {" "}
         Protected route.
